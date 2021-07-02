49bc332bef706db073e30ac8c4eb980452658a2b
roshanakak <roshanak.akbarifar@gmail.com>
2021-07-01 20:49:18 -0400

    tests added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,15 @@
 const request = require('request');
-let args = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, function(err, res, body) {
-  if (err) console.log(err.message);
-  else {
-    const data = JSON.parse(body);
-    if (data[0]) console.log(data[0].description);
-    else console.log('Mentioned breed not found!');
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(err, res, body) {
+    if (err) {
+      callback(`Failure: ${err}`, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data[0]) callback(null, data[0].description);
+      else callback(`Mentioned breed: ${breedName} not found!`, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
